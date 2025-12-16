9f93d14f56c611ee78e346038b55749011af228a
El Ricco <103359779+mandresy-ricco@users.noreply.github.com>
2025-12-16 15:06:45 +0100

    feat : add new function

--- add.js
+++ add.js
@@ -1,5 +1,12 @@
 function calculerAddition(prixPlat, prixBoisson) {
-    return prixPlat + prixBoisson+1;
+    return prixPlat + prixBoisson;
 }
 
-module.exports = calculerAddition;
+function appliquerReduction(total, pourcentage) {
+    return total - (total * pourcentage) / 100;
+}
+
+module.exports = {
+    calculerAddition,
+    appliquerReduction,
+};
